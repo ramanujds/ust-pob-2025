91e0f11bf92dc591344052b30551653376cc6151
ramanujds <ramanujds9@gmail.com>
2025-09-03 19:53:39 +0530

    Java Lambdas and Streams todos

--- java-8/src/main/java/com/ust/model/Task.java
+++ java-8/src/main/java/com/ust/model/Task.java
@@ -7,6 +7,52 @@ public class Task {
     private boolean completed;
     private int priority;
 
+    public Task(int id, String title, boolean completed, int priority) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.priority = priority;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                ", priority=" + priority +
+                '}';
+    }
 }
--- java-8/src/main/java/com/ust/streams/TaskManagementWithStreams.java
+++ java-8/src/main/java/com/ust/streams/TaskManagementWithStreams.java
@@ -4,26 +4,64 @@ import com.ust.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.ToDoubleBiFunction;
 
 public class TaskManagementWithStreams {
 
     List<Task> todos = new ArrayList<>();
 
-    public Task findById(int id){
+    public void addTodos(){
+        Task t1 = new Task(1,"Learn Java Collections",true,5);
+        Task t2 = new Task(2, "Learn Applet",false,1);
+        Task t3 = new Task(3,"Learn Lambda Expression",true,5);
+        Task t4 = new Task(4,"Learn Multi Threading",false,4);
+        Task t5 = new Task(5,"File Handling",false,3);
 
+        todos.addAll(List.of(t1,t2,t3,t4,t5));
+
+    }
+
+    public Task findById(int id){
+           return todos.stream().filter(t->t.getId()==id).findFirst().get();
     }
 
     public List<Task> getPendingTask(){
-
+        return todos.stream().filter(t->!t.isCompleted()).toList();
     }
 
     public List<Task> getCompletedTask(){
-
+        return todos.stream().filter(t->t.isCompleted()).toList();
     }
 
     // priority >= 4
     public List<Task> getImportantPendingTasks(){
+        return todos.stream().filter(t->!t.isCompleted()).filter(t->t.getPriority()>=4).toList();
+    }
 
+    public List<Task> completeAllThePendingTask(){
+
+    }
+
+    public static void main(String[] args) {
+        TaskManagementWithStreams taskManagement = new TaskManagementWithStreams();
+
+        taskManagement.addTodos();
+
+        System.out.println("Pending Tasks : ");
+        printTasks(taskManagement.getPendingTask());
+
+        System.out.println("Completed Tasks : ");
+        printTasks(taskManagement.getCompletedTask());
+
+        System.out.println("Important Pending Tasks : ");
+        printTasks(taskManagement.getImportantPendingTasks());
+
+    }
+
+   static void printTasks(List<Task> tasks){
+        for (Task t:tasks){
+            System.out.println(t);
+        }
     }
 
 }
